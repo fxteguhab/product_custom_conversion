9c8d7de431c1c7a88d8f65287843149d3ed1ed80
aaelsldehoc <aaelsldehoc@gmail.com>
2017-08-02 10:27:42 +0700

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# noinspection PyStatementEffect
+{
+	'name': "Product Custom Conversion",
+	'summary': """
+		Product Custom Conversion
+	""",
+	'description': """
+		Product Custom Conversion
+	""",
+	'author': 'Christyan Juniady and Associates',
+	'maintainer': 'Christyan Juniady and Associates',
+	'website': 'http://www.chjs.biz',
+	'category': 'Uncategorized',
+	'version': '0.1',
+	'depends': ['base'],
+	'data': [
+	],
+	'demo': [
+	],
+	'installable': True,
+	'auto_install': True,
+}
