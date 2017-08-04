f291c9d07d7389b8e2d7fbb62b30f2618fea19d1
Antonius <antoniusa15a@gmail.com>
2017-08-04 15:27:34 +0700

    product conversion

--- conversion.py
+++ conversion.py
@@ -1,6 +1,11 @@
 from openerp.osv import osv, fields
 from openerp.tools.translate import _
 
+_APPLIED_TO = [
+	('product', 'Product'),
+	('category', 'Product Category'),
+]
+
 
 # ==========================================================================================================================
 
@@ -8,11 +13,42 @@ class product_conversion(osv.osv):
 	_name = "product.conversion"
 	_description = "Product Conversion"
 	
-	# COLUMNS ------------------------------------------------------------------------------------------------------------------
+# COLUMNS ------------------------------------------------------------------------------------------------------------------
 	
 	_columns = {
-		'product_id': fields.many2one('product.product', 'Product'),
-		'product_category_id': fields.many2one('product.category', 'Product Category'),
-		'qty': fields.float('Qty.'),
-		'uom_id': fields.many2one('product.uom', 'UoM'),
+		'product_id': fields.many2one('product.template', 'Product', ondelete='cascade'),
+		'product_category_id': fields.many2one('product.category', 'Product Category', ondelete='cascade'),
+		'conversion': fields.float('Qty.', required=True),
+		'uom_id': fields.many2one('product.uom', 'UoM', required=True, ondelete='cascade'),
+		'applied_to' :fields.selection(_APPLIED_TO, 'Applied To', required=True,
+			help='Whether this conversion is for product or product category'),
 	}
+	
+# DEFAULTS ------------------------------------------------------------------------------------------------------------------
+	
+	_defaults = {
+		'applied_to': 'product',
+	}
+	
+# CONSTRAINTS ---------------------------------------------------------------------------------------------------------------
+	
+	def _check_applied_to_product(self, cr, uid, ids, context=None):
+		users_obj = self.pool.get('res.users')
+		user_branch_id = users_obj.browse(cr, uid, [uid], context).branch_id.id
+		for conversion in self.browse(cr, uid, ids, context):
+			if conversion.applied_to == 'product' and not conversion.product_id:
+				return False
+		return True
+	
+	def _check_applied_to_product_category(self, cr, uid, ids, context=None):
+		users_obj = self.pool.get('res.users')
+		user_branch_id = users_obj.browse(cr, uid, [uid], context).branch_id.id
+		for conversion in self.browse(cr, uid, ids, context):
+			if conversion.applied_to == 'category' and not conversion.product_category_id:
+				return False
+		return True
+	
+	_constraints = [
+		(_check_applied_to_product, _('You must input field product if you applied to product'), ['applied_to', 'product_id']),
+		(_check_applied_to_product_category, _('You must input field product category if you applied to product category'), ['applied_to', 'product_category_id']),
+	]
