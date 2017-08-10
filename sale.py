226f621f1ce8890cb2212fbf5c0ce73698e29e12
Antonius <antoniusa15a@gmail.com>
2017-08-10 11:07:22 +0700

    SAle DOne

--- sale.py
+++ sale.py
@@ -30,28 +30,7 @@ class sale_order_line(osv.osv):
 		return nett_price
 	
 # OVERRIDES ---------------------------------------------------------------------------------------------------------------
-	
-	def product_id_change_with_wh(self, cr, uid, ids, pricelist, product, qty=0,
-			uom=False, qty_uos=0, uos=False, name='', partner_id=False,
-			lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, warehouse_id=False, context=None):
-		result = super(sale_order_line, self).product_id_change_with_wh(
-			cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name, partner_id,
-			lang, update_tax, date_order, packaging, fiscal_position, flag, warehouse_id, context=None)
-		product_obj = self.pool.get('product.product')
-		product_conversion_obj = self.pool.get('product.conversion')
-		product_record = product_obj.browse(cr, uid, product)
-		price_unit = product_record.list_price
-		uom_qty = product_conversion_obj.get_conversion_qty(cr, uid, product, uom, qty)
-		nett_price = self._calculate_nett_price(cr, uid, price_unit, product, uom, qty)
-		subtotal = nett_price * uom_qty
-		if qty == 0 : qty = 1
-		
-		result['value'].update({
-			'price_unit': product_record.list_price * uom_qty/qty,
-			'product_uom': uom,
-			'price_subtotal': subtotal
-		})
-		return result
+
 
 	def product_id_change(self, cr, uid, ids, pricelist, product, qty=0,
 			uom=False, qty_uos=0, uos=False, name='', partner_id=False,
@@ -85,5 +64,26 @@ class sale_order_line(osv.osv):
 				'price_subtotal': subtotal
 			}
 		}
+		
+	def onchange_product_uom_qty(self, cr, uid, ids, pricelist, product, qty=0,
+			uom=False, qty_uos=0, uos=False, name='', partner_id=False,
+			lang=False, update_tax=True, date_order=False, packaging=False, fiscal_position=False, flag=False, warehouse_id=False, price_unit = False,context=None):
+		result = super(sale_order_line, self).product_id_change_with_wh(
+			cr, uid, ids, pricelist, product, qty, uom, qty_uos, uos, name, partner_id,
+			lang, update_tax, date_order, packaging, fiscal_position, flag, warehouse_id, context=None)
+		product_obj = self.pool.get('product.product')
+		product_conversion_obj = self.pool.get('product.conversion')
+		product_record = product_obj.browse(cr, uid, product)
+		uom_qty = product_conversion_obj.get_conversion_qty(cr, uid, product, uom, qty)
+		nett_price = self._calculate_nett_price(cr, uid, price_unit, product, uom, qty)
+		subtotal = nett_price * uom_qty
+		if qty == 0 : qty = 1
+		
+		result['value'].update({
+			'price_unit': product_record.list_price * uom_qty/qty,
+			'product_uom': uom,
+			'price_subtotal': subtotal
+		})
+		return result
 		
 # ===========================================================================================================================
